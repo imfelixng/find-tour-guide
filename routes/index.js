3fe99bb1ca0c0cd55bd162fc6cba954a7f356c82
Hoang <vanhoang2604@gmail.com>
2019-02-14 14:48:58 +0700

    modify home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Find tour guide' });
+  res.render('home', { title: 'Find tour guide', jjj: 'a' });
 });
 
 module.exports = router;
