fd35ff93f6bda55578a3748393d2b87f69444d44
Nguyen Quang An <ngquangan@gmail.com>
2019-02-14 14:44:17 +0700

    Create route login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
@@ -20,6 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const Account = require('../models/account');
+
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
   res.render('login');
+});
+
+router.post('/login', async function(req, res, next) {
+  const { username, password } = req.body;
+
+  let account = null;
+  try {
+    account = await Account.findOne({username});
+  } catch (error) {
+    console.log(error);
+  }
+
+  if(!account) {
+    return res.render('login', { error : "Username or password is incorrect!"});
+  }
+
+  if (account.password !== password) {
+    return res.render('login', { error : "Username or password is incorrect!"});
+  }
+  return res.redirect('/');
 });
 
 router.get('/register', function(req, res, next) {
