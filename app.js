b4fa4d65db5142dc54c0c3a15c57398146eefce9
Nguyen Quang An <ngquangan@gmail.com>
2019-02-15 09:53:08 +0700

    create static uploads folder

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
